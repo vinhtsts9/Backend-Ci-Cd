1e016c747eaf426b26cf6a0c959bf49c2432b1d0
vinhtsts9 <vinhtiensinh18@gmail.com>
2024-11-13 22:17:12 +0700

    k

--- src/services/apikey.service.js
+++ src/services/apikey.service.js
@@ -1,8 +1,8 @@
 const apiKeyModel = require('../models/apikey.model')
 const crypto = require('crypto')
 const findById = async (key) => {
-    // const newKey = await apiKeyModel.create({ key: crypto.randomBytes(64).toString('hex'), permissions: ['0000'] })
-    // console.log(newKey)
+   const newKey = await apiKeyModel.create({ key: crypto.randomBytes(64).toString('hex'), permissions: ['0000'] })
+   console.log(newKey)
     try {
         const objKey = await apiKeyModel.findOne({ key, status: true }).lean()
         return objKey
